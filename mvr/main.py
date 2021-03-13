58c810f07618c76b24dc53ca0a22d70a996f4c9a
AdamMusa <adammusa2222@gmail.com>
2021-03-13 21:37:43 +0100

    create init command to initialize project

--- mvr/main.py
+++ mvr/main.py
@@ -2,10 +2,19 @@ import os
 import typer
 
 app = typer.Typer()
-
+files = ["models.py","views.py","routes.py","database.py"]
 @app.command()
 def init(name: str):
     os.mkdir(os.path.join(os.getcwd(),name))
+    os.mkdir(os.path.join(os.getcwd(),name+"/fastapi"))
+    with open(f"./{name}/fastapi/__init__.py",'w') as file:
+        pass
+    for file_name in files:
+        with open(f"./{name}/{file_name}",'w') as file:
+            for i in range(len(files)+1):
+                file.write("print('hello world')")
+                break
+    
     typer.echo(f"Created project {name} successfully.\ncd {name}")
 
 @app.command()
