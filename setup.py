a7d82f276371ad2a35069aa93ad0517fbfb1893c
AdamMusa <adammusa2222@gmail.com>
2021-03-15 23:28:44 +0100

    added mit licence

--- setup.py
+++ setup.py
@@ -8,18 +8,20 @@ setup(
     author="AdamMusa",
     author_email="<adammusa2222@gmail.com>",
     description="this package is inspired by, the mvc architecture",
-    long_description="",
-    url =""
+    long_description=open("README.txt").read() +"\n\n"+open("CHANGELOG.txt").read(),
+    url ="",
+    license = "MIT",
     packages=find_packages(),
     install_requires=["fastapi","uvicorn"], # add any additional packages that 
     # needs to be installed along with your package. Eg: 'caer'
     
-    keywords=['python', 'first package'],
+    keywords=['mvr','python mvr','mvr python' 'fastapi mvr','mvr fastapi','mvc for fastapi','goode architecture for fastapi'],
     classifiers= [
         "Development Status :: 5 - beta",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Unix :: Linux ",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3.6+",
     ]
 )
