965dc3d88905f3359bcc067d4861a6a86948827e
AdamMusa <adammusa2222@gmail.com>
2021-03-16 15:54:26 +0100

    added autocompletion and help text

--- fdriver/main.py
+++ fdriver/main.py
@@ -8,8 +8,10 @@ app = typer.Typer()
 
 
 # command to create project
-@app.command()
-def init(name: str):
+@app.command(help="Allowed you to create project")
+def init(name: str=typer.Option(
+        "", help="start project.", autocompletion=complete_name
+    )):
 
     create_folder(name)
     for i in range(2):
@@ -24,16 +26,20 @@ def init(name: str):
 
 
 # command to start to create app modular
-@app.command()
-def startapp(name: str):
+@app.command(help="To start to application in your project")
+def startapp(name: str=typer.Option(
+        "", help="startapp.", autocompletion=complete_name
+    )):
     create_folder(name)
     create_files(name, files)
     typer.echo(f"Created module {name} successfully 🎉🎉🎉", fg=typer.colors.GREEN)
 
 
 # command to remove app
-@app.command()
-def remove(name: str):
+@app.command(help="Remove application")
+def remove(name: str=typer.Option(
+        "", help="for removing file.", autocompletion=complete_name
+    )):
     try:
         shutil.rmtree(os.path.join(os.getcwd(), name))
         typer.echo(f"Removed successfully {name}")
@@ -42,8 +48,10 @@ def remove(name: str):
 
 
 #allow to run fastapi server
-@app.command(help="Run a FastAPI application.")
-def run(prod: bool = typer.Option(False)):
+@app.command(help="Run a FastAPI server.")
+def run(prod: bool = typer.Option(False)=typer.Option(
+        "", help="allowed you to start easily your server.", autocompletion=complete_name
+    )):
     args = []
     if not prod:
         args.append("--reload")
@@ -66,6 +74,16 @@ def create_files(name, files):
         with open(f"./{name}/{list(files.keys())[i]}", 'w') as file:
             file.write(files[list(files.keys())[i]])
 
+
+valid_names = ["init", "startapp", "run","remove"]
+
+
+def complete_name(incomplete: str):
+    completion = []
+    for name in valid_names:
+        if name.startswith(incomplete):
+            completion.append(name)
+    return completion
 
 if __name__ == "__main__":
     app()
